870e30c1d6b35f04f60c8798f6a046f9f0612f53
Seijo Cecilia <willamwei@outlook.com>
2025-05-07 14:25:54 +0800

    [idl] add toString for primitive types

--- src/main/kotlin/org/ntqqrev/saltify/composeidl/Primitive.kt
+++ src/main/kotlin/org/ntqqrev/saltify/composeidl/Primitive.kt
@@ -7,30 +7,35 @@ sealed class Primitive : Type {
 }
 
 object IntType : Primitive() {
+    override fun toString(): String = "int32"
     override fun toKotlinType(): String = "Int"
     override fun toCSharpType(): String = "int"
     override fun toJavaScriptType(): String = "number"
 }
 
 object UIntType : Primitive() {
+    override fun toString(): String = "uint32"
     override fun toKotlinType(): String = "Long"
     override fun toCSharpType(): String = "uint"
     override fun toJavaScriptType(): String = "number"
 }
 
 object LongType : Primitive() {
+    override fun toString(): String = "int64"
     override fun toKotlinType(): String = "Long"
     override fun toCSharpType(): String = "long"
     override fun toJavaScriptType(): String = "bigint"
 }
 
 object StringType : Primitive() {
+    override fun toString(): String = "string"
     override fun toKotlinType(): String = "String"
     override fun toCSharpType(): String = "string"
     override fun toJavaScriptType(): String = "string"
 }
 
 object BooleanType : Primitive() {
+    override fun toString(): String = "boolean"
     override fun toKotlinType(): String = "Boolean"
     override fun toCSharpType(): String = "bool"
     override fun toJavaScriptType(): String = "boolean"
